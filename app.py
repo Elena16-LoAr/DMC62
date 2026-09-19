cb6f606a183fa8631bcb4c018f22c52268abf50d
Elena16-LoAr <elenaloayzaarteaga336@gmail.com>
2026-09-19 15:55:05 -0500

    Fix variable name casing for lista in app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ if modulos == "Listas":
   valor_inicial = int(st.number_input("Ingresa tu valor inicial del rango", value=0))
   valor_final = int(st.number_input("Ingresa tu valor final del rango",value=10))
 
-  Lista = list(range(valor_inicial, valor_final)) 
+  lista = list(range(valor_inicial, valor_final)) 
   
 elif modulos == "Arreglos":
   st.write("Te encuentras en el módulo de arreglos")
