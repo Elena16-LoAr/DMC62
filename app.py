2cd41617d5f084b41e649b1b69a81b88570cb0e1
Elena16-LoAr <elenaloayzaarteaga336@gmail.com>
2026-09-19 16:15:29 -0500

    Correct message display for 'Listas' module
    Fix typo in the display message for the 'Listas' module.

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import streamlit as st
+import numpy as np
 
 st.title("Especialización Python for Analytics")
 st.sidebar.title("Parámetros")
 st.write("Elaborado por: Elena Loayza")
 
+
+st.image("Python_logo")
+
 modulos = st.sidebar.selectbox("Selecione el módulo",["Listas","Arreglos","Funciones","POO"])
 
 if modulos == "Listas":
-  st.write("Te encuentras en el módulo de listas")
+  st.write(lista)("Te encuentras en el módulo de listas")
 
   valor_inicial = int(st.number_input("Ingresa tu valor inicial del rango", value=0))
   valor_final = int(st.number_input("Ingresa tu valor final del rango",value=10))
