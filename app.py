e86efbdb33144c7a0794049d275eb7b29cb02aa2
Elena16-LoAr <elenaloayzaarteaga336@gmail.com>
2026-09-19 15:34:28 -0500

    Add module selection functionality in app.py

--- app.py
+++ app.py
@@ -5,3 +5,15 @@ st.sidebar.title("Parámetros")
 st.write("Elaborado por: Elena Loayza")
 
 modulos = st.sidebar.selectbox("Selecione el módulo",["Listas","Arreglos","Funciones","POO"])
+
+if modulos == "Listas":
+  st.write("Te encuentras en el módulo de listas")
+
+elif modulos == "Arreglos":
+  st.write("Te encuentras en el módulo de arreglos")
+
+elif modulos == "Funciones":
+   st.write("Te encuentras en el módulo de Funciones")
+
+else:
+   st.write("Te encuentras en el módulo de POO")
